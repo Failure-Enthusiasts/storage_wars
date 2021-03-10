fe965734f0fd8527571fc6be0f35d3a6d0f4cc4f
djchinia <dan.chiniara@datadoghq.com>
2021-03-09 20:33:16 -0500

    add Docker Desktop check

--- run_script.sh
+++ run_script.sh
@@ -1,14 +1,28 @@
+# based off: https://docs.docker.com/network/network-tutorial-standalone/ 
+
+# check if docker is up or not
+result=$( docker ps )
+
+if [[ -n "$result" ]];
+then
+  echo $result
+  echo "Docker is up"
+else
+  echo $result
+  echo "Docker isn't up yet"
+  open -a Docker
+  sleep 45 # wait 45s (maybe we can shorten it)
+fi
+
 docker network create --driver bridge alpine-net
 
 docker run -dit --name alpine1 --network alpine-net alpine ash
 docker run -dit --name alpine2 --network alpine-net alpine ash
 docker run -dit --name alpine3 alpine ash
 
+# check that everything is up
 docker container ls
 docker network ls
-# echo 'before stop'
-# docker stop $(docker ps -a -q)
-# docker rm $(docker ps -a -q)
 
 docker container attach alpine1
 ping -c 2 alpine2
