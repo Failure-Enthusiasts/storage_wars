d8b39de3b8b9cd070acddaf0f06ed5eda1906a74
djchinia <dan.chiniara@datadoghq.com>
2021-04-13 21:00:30 -0400

    chanage var names 2

--- battleship-dan.py
+++ battleship-dan.py
@@ -3,14 +3,14 @@ import random;
 
 grid_width = 3
 grid_height = 3
-bomb_number = 4
-minesArray = np.zeros(shape=(grid_height,grid_width))
+ships_placed = 4
+array = np.zeros(shape=(grid_height,grid_width))
 
-while bomb_number > 0:
+while ships_placed > 0:
   i = random.randint(0,grid_width-1)
   j = random.randint(0,grid_height-1)
-  if (minesArray[j][i] == 0):
-    minesArray[j][i] = 1
-    bomb_number = bomb_number - 1
+  if (array[j][i] == 0):
+    array[j][i] = 1
+    ships_placed = ships_placed - 1
 
-print(minesArray)
+print(array)
